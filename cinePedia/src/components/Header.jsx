973ec8d89003e81cc2ab8dddbc94033e33802862
Hardik Dhamija <hardikdhamija96@gmail.com>
2024-06-13 15:22:08 +0530

    darkMode Integration

--- cinePedia/src/components/Header.jsx
+++ cinePedia/src/components/Header.jsx
@@ -1,19 +1,38 @@
-import logo from "../assets/images/logo/logo.png"
+import { useEffect, useState } from "react";
+import logo from "../assets/images/logo/logo.png";
 import { Link, NavLink, useNavigate } from "react-router-dom";
 
 export const Header = () => {
   const navigate = useNavigate();
 
-  const activeClass = "block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500";
-  const inActiveClass = "block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
+  const [hidden, setHidden] = useState(true);
+  const [darkMode, setDarkMode] = useState(JSON.parse(localStorage.getItem("darkMode")) || false);
 
-  const handleSubmit=(e)=>{
+  useEffect(() => {
+    localStorage.setItem("darkMode", JSON.stringify(darkMode));
+
+    if(darkMode)
+      {
+        document.documentElement.classList.add("dark");
+      }
+      else{
+        document.documentElement.classList.remove("dark");
+      }
+    
+  }, [darkMode]);
+
+  const activeClass =
+    "block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500";
+  const inActiveClass =
+    "block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
+
+  const handleSubmit = (e) => {
     e.preventDefault();
     const queryTerm = e.target.search.value;
     e.target.reset();
 
     return navigate(`/search?q=${queryTerm}`);
-  }
+  };
 
   return (
     <header>
@@ -22,17 +41,43 @@ export const Header = () => {
           <Link
             to="/"
             className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img
-              src={logo}
-              className="h-14"
-              alt="CinePedia Logo"
-            />
+            <img src={logo} className="h-14" alt="CinePedia Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               CinePedia
             </span>
           </Link>
-          <div className="flex md:order-2">
+
+          <div className="flex md:order-2" id="mobile-icons">
             <button
+              onClick={() => setDarkMode(!darkMode)}
+              type="button"
+              className="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+              {darkMode ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 24 24">
+                  <path
+                    fill="currentColor"
+                    d="M12 19a1 1 0 0 1 .993.883L13 20v1a1 1 0 0 1-1.993.117L11 21v-1a1 1 0 0 1 1-1m6.313-2.09l.094.083l.7.7a1 1 0 0 1-1.32 1.497l-.094-.083l-.7-.7a1 1 0 0 1 1.218-1.567zm-11.306.083a1 1 0 0 1 .083 1.32l-.083.094l-.7.7a1 1 0 0 1-1.497-1.32l.083-.094l.7-.7a1 1 0 0 1 1.414 0M4 11a1 1 0 0 1 .117 1.993L4 13H3a1 1 0 0 1-.117-1.993L3 11zm17 0a1 1 0 0 1 .117 1.993L21 13h-1a1 1 0 0 1-.117-1.993L20 11zM6.213 4.81l.094.083l.7.7a1 1 0 0 1-1.32 1.497l-.094-.083l-.7-.7A1 1 0 0 1 6.11 4.74zm12.894.083a1 1 0 0 1 .083 1.32l-.083.094l-.7.7a1 1 0 0 1-1.497-1.32l.083-.094l.7-.7a1 1 0 0 1 1.414 0M12 2a1 1 0 0 1 .993.883L13 3v1a1 1 0 0 1-1.993.117L11 4V3a1 1 0 0 1 1-1m0 5a5 5 0 1 1-4.995 5.217L7 12l.005-.217A5 5 0 0 1 12 7"></path>
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 256 256">
+                  <g fill="currentColor">
+                    <path
+                      d="M227.89 147.89A96 96 0 1 1 108.11 28.11a96.09 96.09 0 0 0 119.78 119.78"
+                      opacity={0.2}></path>
+                    <path d="M233.54 142.23a8 8 0 0 0-8-2a88.08 88.08 0 0 1-109.8-109.8a8 8 0 0 0-10-10a104.84 104.84 0 0 0-52.91 37A104 104 0 0 0 136 224a103.1 103.1 0 0 0 62.52-20.88a104.84 104.84 0 0 0 37-52.91a8 8 0 0 0-1.98-7.98m-44.64 48.11A88 88 0 0 1 65.66 67.11a89 89 0 0 1 31.4-26A106 106 0 0 0 96 56a104.11 104.11 0 0 0 104 104a106 106 0 0 0 14.92-1.06a89 89 0 0 1-26.02 31.4"></path>
+                  </g>
+                </svg>
+              )}
+            </button>
+            {/* <button
               type="button"
               data-collapse-toggle="navbar-search"
               aria-controls="navbar-search"
@@ -53,8 +98,8 @@ export const Header = () => {
                 />
               </svg>
               <span className="sr-only">Search</span>
-            </button>
-            <div className="relative hidden md:block">
+            </button> */}
+            <div className={`relative hidden md:block`}>
               <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                 <svg
                   className="w-4 h-4 text-gray-500 dark:text-gray-400"
@@ -73,14 +118,14 @@ export const Header = () => {
                 <span className="sr-only">Search icon</span>
               </div>
               <form onSubmit={handleSubmit}>
-              <input
-                type="text"
-                id="search-navbar"
-                name="search"
-                className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Search..."
-                autoComplete="off"
-              />
+                <input
+                  type="text"
+                  id="search-navbar"
+                  name="search"
+                  className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Search..."
+                  autoComplete="off"
+                />
               </form>
             </div>
             <button
@@ -88,7 +133,8 @@ export const Header = () => {
               type="button"
               className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               aria-controls="navbar-search"
-              aria-expanded="false">
+              aria-expanded="false"
+              onClick={() => setHidden(!hidden)}>
               <span className="sr-only">Open main menu</span>
               <svg
                 className="w-5 h-5"
@@ -107,7 +153,9 @@ export const Header = () => {
             </button>
           </div>
           <div
-            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            className={`items-center justify-between ${
+              hidden ? "hidden" : ""
+            } w-full md:flex md:w-auto md:order-1`}
             id="navbar-search">
             <div className="relative mt-3 md:hidden">
               <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -127,21 +175,23 @@ export const Header = () => {
                 </svg>
               </div>
               <form onSubmit={handleSubmit}>
-              <input
-                type="text"
-                id="search-navbar"
-                name="search"
-                className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Search..."
-                autoComplete="off"
-              />
+                <input
+                  type="text"
+                  id="search-navbar"
+                  name="search"
+                  className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Search..."
+                  autoComplete="off"
+                />
               </form>
             </div>
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <NavLink
                   to="/"
-                  className={({isActive})=> isActive? activeClass : inActiveClass}
+                  className={({ isActive }) =>
+                    isActive ? activeClass : inActiveClass
+                  }
                   end>
                   Home
                 </NavLink>
@@ -149,25 +199,30 @@ export const Header = () => {
               <li>
                 <NavLink
                   to="/movies/popular"
-                  className={({isActive})=> isActive? activeClass : inActiveClass}>
+                  className={({ isActive }) =>
+                    isActive ? activeClass : inActiveClass
+                  }>
                   Popular
                 </NavLink>
               </li>
               <li>
                 <NavLink
                   to="/movies/top"
-                  className={({isActive})=> isActive? activeClass : inActiveClass}>
+                  className={({ isActive }) =>
+                    isActive ? activeClass : inActiveClass
+                  }>
                   Top Rated
                 </NavLink>
               </li>
               <li>
                 <NavLink
                   to="/movies/upcoming"
-                  className={({isActive})=> isActive? activeClass : inActiveClass}>
+                  className={({ isActive }) =>
+                    isActive ? activeClass : inActiveClass
+                  }>
                   Upcoming
                 </NavLink>
               </li>
-
             </ul>
           </div>
         </div>
--- cinePedia/tailwind.config.js
+++ cinePedia/tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    // colors:{
+    //   darkBg: '#7091e6',
+    //   darkBgShade: '##3d52a0',
+    //   lightBg:'#EDE8F5',
+    //   darkText:'#8697c4',
+    //   lighterDarkText:'#adbbda',
+    //   lightText:'#f5f5f5'
+    // },
+    extend: {
+      
+    },
   },
   plugins: [],
 };
