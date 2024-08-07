e7283c66624bd9ce69def9bde68c9049485497df
Hardik Dhamija <hardikdhamija96@gmail.com>
2024-08-08 05:26:23 +0530

    Ensure all necessary files are committed

--- cinePedia/src/routes/AllRoutes.jsx
+++ cinePedia/src/routes/AllRoutes.jsx
@@ -5,7 +5,7 @@ export const AllRoutes = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<MovieList apiPath="/movie/now_playing" title="Home"/> } />
+        <Route path="/" element={<MovieList apiPath="movie/now_playing" title="Home"/> } />
         <Route path="/movie/:id" element={<MovieDetail />} />
         <Route path="/movies/popular" element={<MovieList apiPath="movie/popular" title="Popular"/>} />
         <Route path="/movies/top" element={<MovieList apiPath="movie/top_rated" title="Top Rated"/>} />
